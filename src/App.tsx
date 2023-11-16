8ab427532b5159dcfcc3060f3122f9680be87503
Gandals-hash <phiritshepiso99@gmail.com>
2023-11-16 13:19:53 +0200

    feat(styling: configure responsive classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import Home from "./home/home";
 import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div>
+    <div className="">
       <Navbar />
-      <main className="flex h-screen flex-col bg-off-white w-full max-w-full"></main>
+      <main className="xl:pl-20 pl-6 overflow-hidden flex h-screen flex-col bg-off-white w-full max-w-full">
+        <Home />
+      </main>
     </div>
   );
 }
