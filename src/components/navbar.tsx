9036397f8b04adc38c6036524ab7aa28ffdc9a4e
Gandals-hash <phiritshepiso99@gmail.com>
2023-11-16 13:19:19 +0200

    feat(styling): configure responsive classes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div className="w-full bg-white z-[2] flex flex-row justify-between items-center md:px-20 xl:px-20 gap-6 h-20 sticky top">
       <img src="/images/logo.svg" alt="Easy Bank Logo" className="md:w-40" />
-      <div className="flex flex-row gap-6 text-gray">
+      <div className="flex flex-row gap-6 2xl:gap-12 text-gray">
         <a href="/home">Home</a>
         <a href="/about">About</a>
         <a href="/contact">Contact</a>
