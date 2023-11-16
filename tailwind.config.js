b3e41ba5ff7cb4cb6456294fd81806e850cb14e6
Gandals-hash <phiritshepiso99@gmail.com>
2023-11-16 10:37:06 +0200

    feat: page fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "primary-color": " #95f9c3",
+      "secondary-color": "#31cd7a",
+      white: "#ffffff",
+      gray: "#808080",
+      "off-white": "	#f9f9f9",
+    },
   },
   plugins: [],
 };
