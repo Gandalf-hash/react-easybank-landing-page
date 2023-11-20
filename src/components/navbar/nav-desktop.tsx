9f2e730e73366b23be95aec605f502170e62e295
Gandals-hash <phiritshepiso99@gmail.com>
2023-11-20 12:23:47 +0200

    feat: make navbar always sticky

--- src/components/navbar/nav-desktop.tsx
+++ src/components/navbar/nav-desktop.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 import PrimaryButton from "../button/primary-button";
 import { routes } from "../../routes.util";
 import { Logo } from "../logo/logo";
 
 export const NavDesktop = () => {
+  const [isSticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.scrollY;
+      if (offset > 0) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="hidden w-full bg-white z-[2] lg:flex flex-row justify-between items-center md:px-20 xl:px-20 gap-6 h-20 sticky top">
+    <div className="hidden w-full bg-white z-[20] lg:flex flex-row justify-between items-center md:px-32 gap-6 h-20 sticky top">
       <Logo />
       <div className="flex flex-row gap-6 2xl:gap-12 text-gray">
         {routes.map((route) => (
